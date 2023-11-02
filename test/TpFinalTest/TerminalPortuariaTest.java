c83f3679a0245a6900fcc91ff17608c6525397da
LorenzoFerraces <lorenzoferraces@proton.me>
2023-11-02 17:43:10 -0300

    test corregidos usando mockito

--- test/TpFinalTest/TerminalPortuariaTest.java
+++ test/TpFinalTest/TerminalPortuariaTest.java
@@ -1,6 +1,7 @@
 package TpFinalTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,9 +19,11 @@ class TerminalPortuariaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		this.p1 = new Punto(5d,10d);
-		this.p2 = new Punto(7d,12d);
-		
+		this.p1 = mock(Punto.class);
+//		this.p1 = new Punto(5d,10d);
+		this.p2 = mock(Punto.class);
+//		this.p2 = new Punto(7d,12d);
+		when(this.p1.calcularDistancia(p2)).thenReturn(Math.sqrt(8));
 		this.t1 = new TerminalPortuaria(p1);
 		this.t2 = new TerminalPortuaria(p2);
 	}
