39c15515bfca91bf28422e28f555759cab6f5d13
LorenzoFerraces <lorenzoferraces@proton.me>
2023-11-08 22:36:39 -0300

    metodos de agregado de circuitos

--- src/naviera/circuitoMaritimo/CircuitoMaritimo.java
+++ src/naviera/circuitoMaritimo/CircuitoMaritimo.java
@@ -52,26 +52,42 @@ public class CircuitoMaritimo {
 		return tramos.remove(tramos.size() - 1);
 	}
 	
-	private List<Tramo> subCircuito(TerminalPortuaria inicio, TerminalPortuaria fin) {
-//		TODO: implementar buscador por terminal de origen y buscador por terminal de destino
-		return new ArrayList<Tramo>();
+	private CircuitoMaritimo subCircuito(TerminalPortuaria inicio, TerminalPortuaria fin) {
+//		Prop: describe el CircuitoMaritimo con 'inicio' como terminal de origen, y 'fin' como terminal de fin
+//			  En caso de no existir alguna de las terminales, retorna un circuito vacio			  
+		try {
+			return new CircuitoMaritimo(tramos.subList(0, 0));
+		} catch (Exception e) {
+			return new CircuitoMaritimo();
+		}
 	}
 	
 	public boolean contieneTerminal(TerminalPortuaria t) {
-//		devuelve si un circuito contiene una terminal t, en cualquier parte del recorrido
+//		Prop: indica si el circuito contiene la terminal como origen o destino en alguno de sus tramos
 		return tramos.parallelStream().map(tramo -> tramo.contieneTerminal(t)).anyMatch(bool -> bool);
 	}
 	
 	public Double tiempoTotal() {
+//		Prop: describe el tiempo total de recorrido de los tramos
 		return this.mapReduceToDouble(Tramo::getTiempo);
 	}
 	
 	public Double precioTotal() {
+//		Prop: describe el total del costo de los tramos
 		return this.mapReduceToDouble(Tramo::getPrecio);
 	}
 	
 	private Double mapReduceToDouble(ToDoubleFunction<Tramo> f) {
 		return tramos.stream().mapToDouble(f).sum();
+	}
+	
+	private int posicionDe(TerminalPortuaria t){
+		
+		
+		for(Tramo tramo : tramos) {
+			
+		}
+		return 
 	}
 	
 	
