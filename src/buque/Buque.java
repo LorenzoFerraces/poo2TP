ef30b58889064c9b7c9617ddd3db8217f5ada913
Santino <Santino@DESKTOP-I0QDC7B>
2023-11-10 23:43:03 -0300

    Creada la clase Buque

---
+++ src/buque/Buque.java
@@ -0,0 +1,31 @@
+package buque;
+
+import java.util.List;
+
+import TpFinal.TerminalPortuaria;
+import containers.Container;
+import faseDeBuque.*;
+
+public class Buque {
+
+	private List<Container> carga;
+	private GPS gps;
+	private FaseDeBuque fase_del_buque;
+	private TerminalPortuaria terminal;
+	
+	public Buque(List<Container> carga, GPS gps, TerminalPortuaria terminal) {
+		super();
+		this.carga = carga;
+		this.gps = gps;
+		this.fase_del_buque = new FaseDeBuqueOutbound();
+		this.terminal = terminal;
+	}
+
+	public void cambiarFase() {
+		this.fase_del_buque.siguienteFase();
+	}
+	
+	public Punto posicion() {
+		
+	}
+}
