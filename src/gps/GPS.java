00672f56b88e01546a92b95bd89470dd99b25bb4
Santino <Santino@DESKTOP-I0QDC7B>
2023-11-16 18:10:58 -0300

    Simulada la navegación

--- src/gps/GPS.java
+++ src/gps/GPS.java
@@ -1,24 +1,62 @@
 package gps;
 
-import TpFinal.Punto;
-import TpFinal.TerminalPortuaria;
+import java.util.Observable;
 
-public class GPS {
+import TpFinal.Punto;
+
+@SuppressWarnings("deprecation")
+public class GPS extends Observable {
 
 	private Punto posicion;
-	private TerminalPortuaria terminal;
 	
-	public GPS(Punto posicion, TerminalPortuaria terminal) {
+	public GPS(Punto posicion) {
 		this.posicion = posicion;
-		this.terminal = terminal;
 	}
 
 	public Punto getPosicion() {
 		return posicion;
 	}
 	
+	// Modifica la posicion y notifica a los observadores
 	public void setPosicion(Punto posicion) {
 		this.posicion = posicion;
+		this.setChanged();
+		this.notifyObservers();
 	}
 
+	// Modifica en 1 la coordenada x e y para simular el acercamiento a la terminal
+	public void acercarseAPosicion(Punto posicionAAcercarse) {
+		Double coordenadaX = posicion.getFirst();
+		Double coordenadaY = posicion.getSecond();
+		if (coordenadaX < posicionAAcercarse.getFirst()) {
+            coordenadaX = coordenadaX + 1;
+        } else if (coordenadaX > posicionAAcercarse.getFirst()) {
+        	coordenadaX = coordenadaX - 1;
+        }
+
+        if (coordenadaY < posicionAAcercarse.getSecond()) {
+        	coordenadaY = coordenadaY + 1;
+        } else if (coordenadaY > posicionAAcercarse.getSecond()) {
+        	coordenadaY = coordenadaY - 1;
+        }
+		this.setPosicion(new Punto(coordenadaX, coordenadaY));
+	}
+
+	// Modifica en 1 la coordenada x e y para simular el alejamiento de la terminal
+	public void alejarseDePosicion(Punto posicionAAlejarse) {
+		Double coordenadaX = posicion.getFirst();
+	    Double coordenadaY = posicion.getSecond();
+	    if (coordenadaX <= posicionAAlejarse.getFirst()) {
+	        coordenadaX = coordenadaX - 1;
+	    } else {
+	        coordenadaX = coordenadaX + 1;
+	    }
+
+	    if (coordenadaY <= posicionAAlejarse.getSecond()) {
+	        coordenadaY = coordenadaY - 1;
+	    } else {
+	        coordenadaY = coordenadaY + 1;
+	    }
+	    this.setPosicion(new Punto(coordenadaX, coordenadaY));
+	}
 }
--- test/gps/GPSTest.java
+++ test/gps/GPSTest.java
@@ -1,34 +1,53 @@
 package gps;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import TpFinal.Punto;
-import TpFinal.TerminalPortuaria;
 
 class GPSTest {
 
 	private GPS gps;
-	private Punto posicion;
-	private TerminalPortuaria terminal;
+	private Punto p1;
+	private Punto p2;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		posicion = mock(Punto.class);
-		gps = new GPS(posicion, terminal);
+		p1 = new Punto(0.0, 0.0);
+		p2 = new Punto(3.0, 4.0);
+		gps = new GPS(p1);
 	}
 
 	@Test
 	void testUnGPSConoceSuPosicion() {
-		fail("Not yet implemented");
+		assertEquals(p1, gps.getPosicion());
 	}
 
 	@Test
-	void testSetPosicion() {
-		fail("Not yet implemented");
+	void testUnGPSPuedeCambiarSuPosicion() {
+		gps.setPosicion(p2);
+		assertEquals(p2, gps.getPosicion());
 	}
 
+	@Test
+	void testUnGPSPuedeAcercarseAUnaPosicion() {
+		// El GPS tiene un punto inicial de 0.0, 0.0
+        // El punto a acercarse es 3.0, 4.0
+        gps.acercarseAPosicion(p2);
+        assertEquals(1, gps.getPosicion().getFirst());
+        assertEquals(1, gps.getPosicion().getSecond());
+	}
+	
+	@Test
+	void testUnGPSPuedeAlejarseDeUnaPosicion() {
+		// El GPS tiene un punto inicial de 0.0, 0.0
+		// El punto a alejarse es 3.0, 4.0
+		
+		gps.alejarseDePosicion(p2);
+        assertEquals(-1, gps.getPosicion().getFirst());
+        assertEquals(-1, gps.getPosicion().getSecond());
+
+	}
 }
