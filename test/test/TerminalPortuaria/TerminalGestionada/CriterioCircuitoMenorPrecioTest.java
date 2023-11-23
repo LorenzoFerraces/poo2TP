c8889fd8908374482d5a1acf5f158f56f07b4f2b
LorenzoFerraces <lorenzoferraces@proton.me>
2023-11-23 03:11:23 -0300

    esqueleto test TerminalGestionada

---
+++ test/test/TerminalPortuaria/TerminalGestionada/CriterioCircuitoMenorPrecioTest.java
@@ -0,0 +1,72 @@
+package test.TerminalPortuaria.TerminalGestionada;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import naviera.circuitoMaritimo.CircuitoMaritimo;
+import terminalPortuaria.TerminalGestionada.TerminalGestionada;
+
+
+class TerminalGestionadaTest {
+	
+	private TerminalGestionada terminal;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testAddNaviera() {}
+	
+	@Test
+	void testAddShipper() {}
+	
+	@Test
+	void testAddConsignee() {}
+	
+	@Test
+	void testAddEmpresa() {}
+	
+	@Test
+	void testAddConductor() {}
+	
+	@Test
+	void testAddCamion() {}
+	
+	@Test
+	void testAddOrdenImport() {}
+	
+	@Test
+	void testAddOrdenExport() {}
+	
+	@Test
+	void testSetCriterio() {}
+	
+	@Test
+	void testCalcularMejorCircuito() {}
+	
+	@Test
+	void testViajesConCirtuito() {}
+	
+	@Test
+	void testExportar() {}
+	
+	@Test
+	void testImportar() {}
+	
+	@Test
+	void testproximaSalidaBuque() {}
+	
+	@Test
+	void testfiltrarViajes() {}
+
+	@Test
+	void testCuandoTardaEnLlegar() {}
+}
