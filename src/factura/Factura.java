5787e144a45e2ea26321f7cd5cd5619dca0d40f5
scargnelutti <scargnelutti@protonmail.com>
2023-11-30 23:44:31 -0300

    Finalizadas las facturas

--- src/factura/Factura.java
+++ src/factura/Factura.java
@@ -16,31 +16,62 @@ public class Factura {
 		this.fechaDeFacturacion = fechaDeFacturacion;
 	}
 
-	public String imprimirFactura() {
-		List<IServicio> serviciosBrindados = this.orden.getCarga().getServicios();
-		double montoTotalPorServicios = serviciosBrindados.stream().
-				mapToDouble(servicio -> servicio.costo()).sum();
-		double montoTotalPorViaje = this.orden.getViajeElegido().getCircuito().precioTotal();
-		
-		// Encabezado de factura
-		StringBuilder factura = new StringBuilder(this.getClass().getSimpleName() + ": \n" +
-			"Fecha de expedicion: " + fechaDeFacturacion.toString() + "\n" + 
-			"Servicios brindados: " + "\n");
-		
-		// Desglose de servicios
-		serviciosBrindados.forEach(servicio -> 
-			factura.append(servicio.getClass().getSimpleName() + ": " + servicio.costo() + "\n"));
-		factura.append("\n");
-		
-		factura.append("Subtotal Servicios: " + montoTotalPorServicios + "\n");
-		factura.append("Subtotal Viaje: " + montoTotalPorViaje + "\n\n");
-		
-		factura.append("Total a pagar: " + (montoTotalPorServicios + montoTotalPorViaje));
-		
-		return factura.toString().trim();
-	}
-
 	public LocalDateTime getFechaDeFacturacion() {
 		return fechaDeFacturacion;
 	}
+
+	public String facturaParaShipper() {
+		StringBuilder factura = this.parteInicial();
+		
+		factura.append("Total a pagar: " + (montoTotalPorServicios()));
+		return factura.toString().trim();
+	}
+	
+	public String facturaParaConsignee() {
+		StringBuilder factura = this.parteInicial();
+		
+		factura.append("Subtotal Viaje: " + montoTotalPorViaje() + "\n");
+		
+		factura.append("Total a pagar: " + (montoTotalPorServicios() + montoTotalPorViaje()));
+		return factura.toString().trim();
+	}
+	
+	// Auxiliares de factura
+	
+	public double montoTotalPorServicios() {
+		return orden.getCarga().getServicios().stream()
+			.mapToDouble(servicio -> servicio.costo())
+			.sum();
+	}
+	
+	public double montoTotalPorViaje() {
+		return orden.getViajeElegido().getCircuito().precioTotal();
+	}
+	
+	private List<IServicio> serviciosAFacturar() {
+		return orden.getCarga().getServicios();
+	}
+	
+	private StringBuilder encabezadoDeFactura() {
+		StringBuilder encabezado = new StringBuilder(this.getClass().getSimpleName() + ": \n" +
+				"Fecha de expedicion: " + fechaDeFacturacion.toString() + "\n" + 
+				"Servicios brindados: " + "\n");
+		return encabezado;
+	}
+	
+	private StringBuilder parteInicial() {
+		StringBuilder factura = new StringBuilder(this.encabezadoDeFactura());
+		
+		desgloseDeServicios(factura);
+
+		return factura;
+	}
+
+	private void desgloseDeServicios(StringBuilder factura) {
+		serviciosAFacturar().forEach(servicio -> 
+			factura.append(servicio.getClass().getSimpleName() + ": " + servicio.costo() + "\n"));
+		factura.append("\n");
+		factura.append("Subtotal Servicios: " + montoTotalPorServicios() + "\n");
+	}
+
 }
--- test/factura/FacturaTest.java
+++ test/factura/FacturaTest.java
@@ -10,10 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import container.Container;
-import naviera.circuitoMaritimo.CircuitoMaritimo;
+import naviera.viaje.circuitoMaritimo.CircuitoMaritimo;
 import ordenes.Orden;
 import services.ServicioDeLavado;
-import viaje.Viaje;
+import naviera.viaje.Viaje;
 
 class FacturaTest {
 
@@ -38,7 +38,7 @@ class FacturaTest {
 	}
 
 	@Test
-    public void testImprimirFactura() {
+    public void testFacturaParaShipper() {
         // Setup
         when(servicioLavado.costo()).thenReturn(100.0);
         when(circuito.precioTotal()).thenReturn(200.0);
@@ -51,9 +51,38 @@ class FacturaTest {
         String stringEsperado = "Factura: \nFecha de expedicion: " + factura.getFechaDeFacturacion().toString() + "\nServicios brindados: \n" +
             "ServicioDeLavado: 100.0\n\n" +
             "Subtotal Servicios: 100.0\n" +
-            "Subtotal Viaje: 200.0\n\n" + 
+        	"Total a pagar: 100.0\n";
+        assertEquals(stringEsperado.trim(), factura.facturaParaShipper());
+        
+        // String esperado
+        /*
+        Factura: 
+		Fecha de expedicion: 2023-11-22T16:37:45.689723600
+		Servicios brindados: 
+		ServicioDeLavado: 100.0
+
+		Subtotal Servicios: 100.0
+		Total a pagar: 200.0
+         */
+    }
+	
+	@Test
+    public void testFacturaParaConsignee() {
+        // Setup
+        when(servicioLavado.costo()).thenReturn(100.0);
+        when(circuito.precioTotal()).thenReturn(200.0);
+        when(viaje.getCircuito()).thenReturn(circuito);
+        when(carga.getServicios()).thenReturn(Arrays.asList(servicioLavado));
+        when(orden.getCarga()).thenReturn(carga);
+        when(orden.getViajeElegido()).thenReturn(viaje);
+
+        // Verificar el resultado
+        String stringEsperado = "Factura: \nFecha de expedicion: " + factura.getFechaDeFacturacion().toString() + "\nServicios brindados: \n" +
+            "ServicioDeLavado: 100.0\n\n" +
+            "Subtotal Servicios: 100.0\n" +
+            "Subtotal Viaje: 200.0\n" +
         	"Total a pagar: 300.0\n";
-        assertEquals(stringEsperado.trim(), factura.imprimirFactura());
+        assertEquals(stringEsperado.trim(), factura.facturaParaConsignee());
         
         // String esperado
         /*
@@ -64,9 +93,7 @@ class FacturaTest {
 
 		Subtotal Servicios: 100.0
 		Subtotal Viaje: 200.0
-
 		Total a pagar: 300.0
          */
     }
-
 }
